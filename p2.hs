578f7accd87d8855070cfbd19b16027c9987069f
Ever <g.everax@gmail.com>
2015-10-07 00:05:06 +0800

    p2 refactor

--- p2.hs
+++ p2.hs
@@ -2,9 +2,8 @@ module Main where
 
 import Data.List
 
-fib = 1:2:zipWith (+) fib (tail fib)
-
 solve :: Int -> Int
-solve n = sum $ takeWhile (\x -> x < n) $ filter (even) fib
+solve n = sum $ takeWhile (<n) $ filter even fib
+    where fib = 1:2:zipWith (+) fib (tail fib)
 
 main = print $ solve 4000000
